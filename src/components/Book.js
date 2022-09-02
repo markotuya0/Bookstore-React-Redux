03179a8e35817624af5f0004b583bb70e17b6375
Mark Otuya <markotuya0@gmail.com>
2022-09-02 02:58:05 +0100

    fixed nav-bar

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { removeBook } from '../redux/books/books';
+import progressImage from '../images/progress-icon.png';
 
 const Book = ({
   title, author, id, category,
@@ -20,6 +21,22 @@ const Book = ({
       <h2 className="title">{title}</h2>
       <h3 className="author">{author}</h3>
       <button className="delete" type="button" onClick={deleteBook} value={id}>Delete</button>
+
+      <div className="status-container">
+        <dt className="prog-report">
+          <img src={progressImage} className="prog-image" alt="progress-icon" />
+          <div className="status">
+            <dl className="percent">85%</dl>
+            <dl className="complete">Completed</dl>
+          </div>
+        </dt>
+
+        <dt className="chapter-container">
+          <dl className="chapter">CURRENT CHAPTER</dl>
+          <dl className="topic">Chapter 8</dl>
+          <button className="prog-btn" type="button">UPDATE PROGRESS</button>
+        </dt>
+      </div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import contactImage from '../images/contact-icon.png';
 
 const Header = () => (
   <header className="font1">
+    <div className="icon">
+      <img src={contactImage} className="image" alt="contact-icon" />
+    </div>
     <h1>Library</h1>
     <nav>
       <Link to="/">BOOKS</Link>
